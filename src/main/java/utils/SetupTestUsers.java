bbe3d988816cb0626055a842b10fa1bc2ad91cdc
KD131 <71063280+KD131@users.noreply.github.com>
2022-01-12 23:05:18 +0100

    adds populate test users resource

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -9,11 +9,10 @@ import javax.persistence.EntityManagerFactory;
 
 public class SetupTestUsers {
 
-  public static void main(String[] args) {
-
+  public static void populate() {
     EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
     EntityManager em = emf.createEntityManager();
-    
+
     // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
     // CHANGE the three passwords below, before you uncomment and execute the code below
@@ -44,7 +43,10 @@ public class SetupTestUsers {
     System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
     System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
     System.out.println("Created TEST Users");
-   
+  }
+
+  public static void main(String[] args) {
+    populate();
   }
 
 }
