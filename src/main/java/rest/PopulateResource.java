6050f71f87f2d9e2dfc72331ea213904cd2b64b6
KD131 <71063280+KD131@users.noreply.github.com>
2022-01-14 07:27:53 +0100

    adds test data populate endpoint

--- src/main/java/rest/PopulateResource.java
+++ src/main/java/rest/PopulateResource.java
@@ -1,5 +1,6 @@
 package rest;
 
+import utils.Populator;
 import utils.SetupTestUsers;
 
 import javax.ws.rs.GET;
@@ -12,8 +13,17 @@ public class PopulateResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("users")
     public String testUsers() {
         SetupTestUsers.populate();
         return "Successfully populated with test users.";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("data")
+    public String testData() {
+        Populator.populate();
+        return "Successfully populated with test data.";
     }
 }
